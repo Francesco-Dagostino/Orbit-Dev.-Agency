e36e91cc103e95611fe882de2e1e29936c9eb933
LuigiMarconiFavini <luigimarconifavini@gmail.com>
2026-03-25 18:44:19 -0300

    add local IP to CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,8 +14,10 @@ const allowedOrigins = [
   process.env.FRONTEND_URL,
   "http://localhost:5173",
   "http://localhost:4173",
-  "http://127.0.0.1:5173/",  // ← agregar esta línea
-  "http://127.0.0.1:4173/",  // ← y esta
+  "http://127.0.0.1:5173/",
+  "http://127.0.0.1:4173/",
+  "http://192.168.0.244:5173/",  // ← agregar esta
+  "http://192.168.0.244:4173/",  // ← y esta
 ].filter(Boolean);
 
 app.use(cors({
